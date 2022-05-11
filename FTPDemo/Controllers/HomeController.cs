7e93c9f81c3edf0bf8b96babc6bca5b24d6cc93e
PriteeCybercom <pritee.cybercom@gmail.com>
2022-05-11 12:58:39 +0530

    file path changes

---
+++ FTPDemo/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FTPDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string ftpResponse = 
+                DownloadFile("ftp://<FTP_URL>",
+                "<REMOTE_FILE_NAME_TO_DOWNLOAD>", 
+                "<FTP_USERNAME>", 
+                "<FTP_PASSWORD>", "D:\\Pritee\\<LOCAL_PATH>");
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public string DownloadFile(string FtpUrl, string FileNameToDownload,
+                        string userName, string password, string tempDirPath)
+        {
+            string ResponseDescription = "";
+            string PureFileName = new FileInfo(FileNameToDownload).Name;
+            string DownloadedFilePath = tempDirPath + "/" + PureFileName;
+            string downloadUrl = String.Format("{0}/{1}", FtpUrl, FileNameToDownload);
+
+            FtpWebRequest req = (FtpWebRequest)FtpWebRequest.Create(downloadUrl);
+            
+            req.Method = WebRequestMethods.Ftp.DownloadFile;
+            req.Credentials = new NetworkCredential(userName, password);
+            req.UseBinary = true;
+            req.Proxy = null;
+
+            try
+            {
+
+                FtpWebResponse response = (FtpWebResponse)req.GetResponse();
+                Stream stream = response.GetResponseStream();
+
+                byte[] buffer = new byte[2048];
+                FileStream fs = new FileStream(DownloadedFilePath, FileMode.Create);
+                int ReadCount = stream.Read(buffer, 0, buffer.Length);
+                while (ReadCount > 0)
+                {
+                    fs.Write(buffer, 0, ReadCount);
+                    ReadCount = stream.Read(buffer, 0, buffer.Length);
+                }
+                ResponseDescription = response.StatusDescription;
+                fs.Close();
+                stream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return ResponseDescription;
+        }
+    }
+}
---
+++ FTPDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FTPDemo.MvcApplication" Language="C#" %>
